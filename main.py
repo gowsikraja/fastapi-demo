c501649ab31189ccb5e62862a109337eff661f55
gowsikraja <gowsik.andro@gmail.com>
2026-02-10 19:17:04 +0530

    Post method practice

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Movie(BaseModel):
+    id: int
+    name: str
+    type: str
+
 
 myapp = FastAPI()
 
@@ -8,9 +16,19 @@ def index():
     return 'Hello world'
 
 
+@myapp.get('/prperty/{id}')
+def property(id):
+    return f'This is property page of {id}'
+
+
 @myapp.get('/movies')
 def movies():
     return {'movies': [
         {'Movie1', 'action'},
         {'Movie2', 'love'}
     ]}
+
+
+@myapp.post('/movies')
+def getMovies(movie: Movie):
+    return movie
