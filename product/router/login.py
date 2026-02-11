92e54321ec1b44ef97949e0ea11451552f2d2872
gowsikraja <gowsik.andro@gmail.com>
2026-02-11 22:29:42 +0530

    JWT token created

--- product/router/login.py
+++ product/router/login.py
@@ -3,10 +3,26 @@ from ..import schema, model
 from passlib.context import CryptContext
 from ..database import get_db
 from sqlalchemy.orm import Session
+from datetime import datetime, timedelta
+from jose import jwt, JWTError
 
 router = APIRouter(tags=['Login'])
 
 pwd_context = CryptContext(schemes=['argon2'], deprecated='auto')
+
+SECRET_KEY = '5fe6456aaea7d34ed980235568e1478a1848b0c03030ff5268d40957b0647684'
+ALGORITHM = 'HS256'
+EXPIRE_TIME = 20  # mins
+
+
+def get_token(data: dict):
+    to_token = data.copy()
+    expire = datetime.now() + timedelta(minutes=EXPIRE_TIME)
+    to_token.update({
+        'exp': expire
+    })
+    encode_jwt = jwt.encode(to_token, SECRET_KEY, algorithm=ALGORITHM)
+    return encode_jwt
 
 
 @router.post('/login')
@@ -21,5 +37,10 @@ def login(request: schema.LoginRequest, db: Session = Depends(get_db)):
     if not pwd_context.verify(request.password, seller.password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid Username/Password')
-
-    return 'Logged in successfully'
+    token = get_token({
+        'sub': seller.username
+    })
+    return {
+        'message':'Logged-in successfully',
+        'access_token': token
+    }
