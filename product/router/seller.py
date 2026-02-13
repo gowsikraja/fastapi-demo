037a0c237ca32d69727ff3ac4dcb4f62608cf4ab
gowsikraja <gowsik.andro@gmail.com>
2026-02-13 20:55:55 +0530

    Get seller details added

--- product/router/seller.py
+++ product/router/seller.py
@@ -1,16 +1,27 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from ..import schema, model
 from ..database import get_db
 from passlib.context import CryptContext
+from .login import get_user
+from typing import List
 
 router = APIRouter(tags=['Seller'])
 
 pwd_context = CryptContext(schemes=['argon2'], deprecated='auto')
 
 
+@router.get('/seller', response_model=List[schema.DisplaySeller])
+def getSeller(db: Session = Depends(get_db), user: schema.Seller = Depends(get_user)):
+    seller = db.query(model.Seller).all()
+    if not seller:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail='No sellers available')
+    return seller
+
+
 @router.post('/seller')
-def seller(request: schema.Seller, db: Session = Depends(get_db)):
+def seller(request: schema.Seller, db: Session = Depends(get_db), user: schema.Seller = Depends(get_user)):
     hash_psw = pwd_context.hash(request.password)
     seller = model.Seller(username=request.username,
                           email=request.email, password=hash_psw)
